17853d78b68e74ef0657c2da975eb5b6526ae379
Davy Tenn <davy.lsdev@gmail.com>
2015-02-21 22:37:36 -0700

    Checkin initial code

---
+++ WriteHiCScode/WriteHiCScode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;     // DLL support
+
+namespace WriteHiCScode
+{
+	class MainClass
+	{
+		[DllImport("libwriteHi.so")]
+		public static extern void DisplayHiFromDLL ();
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("This is Hi from the C# program");
+			DisplayHiFromDLL ();
+		}
+	}
+}
---
+++ writeHi/writeHi/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "main.h"
+#include <stdio.h>
+
+extern "C"
+{
+ // __declspec(dllexport) void DisplayHelloFromDLL()
+void DisplayHiFromDLL()
+  {
+    printf ("This is Hi from dll !\n");
+  }
+}
